15fa6aecfddf374a2e4bfd010cbb58380124f11d
kuyucucaner <141584156+kuyucucaner@users.noreply.github.com>
2023-08-20 18:44:20 +0300

    Add files via upload

---
+++ bodymassindex/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bodymassindex
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double boy, kilo, vki;
+            boy = Convert.ToDouble(txtBoy.Text);
+            kilo = Convert.ToDouble(txtKilo.Text);
+            vki = kilo / ((boy/100) * (boy / 100));
+            label5.Text = vki.ToString();
+            if (vki < 18)
+            {
+                label6.Text = "Zayıf";
+            }
+            else if (vki >= 18 && vki < 25)
+            {
+                label6.Text = "Normal";
+            }
+            else if (vki >= 25 && vki < 30)
+            {
+                label6.Text = "Kilolu";
+            }
+            else if (vki >= 30 && vki < 35)
+            {
+                label6.Text = "Obez";
+            }
+            else
+            {
+                label6.Text = "Ciddi Obez";
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtBoy_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ bodymassindex/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("bodymassindex")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("bodymassindex")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("3e499c08-8b10-4394-94aa-00fbf16b9fb1")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
